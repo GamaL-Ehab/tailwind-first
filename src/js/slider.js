4c945912c4b9723047f15a6e59483b6fd91d7148
Gamal-Ehab <gamalehabg@gmail.com>
2025-01-27 20:35:02 +0200

    moving index

--- src/js/slider.js
+++ src/js/slider.js
@@ -9,23 +9,23 @@ const images = ["images/ales-nesetril-Im7lZjxeLhg-unsplash-EFNYJM67.jpg",
 ];
 
 let i = 0;
-img.src = images[i];
+img.src = 'src/' + images[i];
 
 prev.addEventListener('click', function(){
     if(i == 0 ){
         i = images.length-1
-        img.src = images[i];
+        img.src = 'src/' + images[i];
     }else{
         i--
-        img.src = images[i];
+        img.src = 'src/' + images[i];
     }  
 })
 next.addEventListener('click', function(){
     if(i == images.length - 1){
-        img.src = images[i];
+        img.src = 'src/' + images[i];
         i = 0
     }else{
         i++
-        img.src = images[i];
+        img.src = 'src/' + images[i];
     }  
 })
